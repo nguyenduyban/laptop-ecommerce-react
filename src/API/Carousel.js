be472d997d474c190df1fc9d0539bafaac6b032d
duyban <duybanit123@hotmail.com>
2025-11-23 20:30:47 +0700

    Update Slideshow

--- src/API/Carousel.js
+++ src/API/Carousel.js
@@ -1,9 +1,18 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://ecommerce-laravel.up.railway.app/api", 
+  baseURL: "https://ecommerce-laravel.up.railway.app/api",
 });
-
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 // Hàm lấy danh sách carosel
 export const getAllCarousel = async () => {
   try {
@@ -22,4 +31,25 @@ export const getCarouselById = async (STT) => {
     console.error("Lỗi khi lấy chi tiết sản phẩm:", error);
     throw error;
   }
+};
+// Thêm slideshow
+export const createCarousel = async (formData) => {
+  const res = await api.post("/admin/slides", formData, {
+    headers: { "Content-Type": "multipart/form-data" },
+  });
+  return res.data;
+};
+
+// Cập nhật slideshow
+export const updateCarousel = async (STT, formData) => {
+  const res = await api.post(`/admin/slides/${STT}?_method=PUT`, formData, {
+    headers: { "Content-Type": "multipart/form-data" },
+  });
+  return res.data;
+};
+
+// Xóa slideshow
+export const deleteCarousel = async (STT) => {
+  const res = await api.delete(`/admin/slides/${STT}`);
+  return res.data;
 };
